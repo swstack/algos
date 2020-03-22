ec093d664b56bf846a35549d00a72d0c14276221
Stephen Stack <sfstack7500@gmail.com>
2020-03-22 18:05:34 -0500

    Leetcode: Max sub array

--- src/leetcode/MaxSubArray.java
+++ src/leetcode/MaxSubArray.java
@@ -6,26 +6,34 @@ public class MaxSubArray {
 	public int maxSubArray(int[] nums) {
 		// Sliding window, if the value of the current index is ever greater
 		// than the value of the current window, the window resets.
+		int max;
 
-		int max = 0;
-		int curMax = nums[0];
-		for (int idxEnd = 1; idxEnd < nums.length; idxEnd++) {
-			int curr = nums[idxEnd];
-			curMax = curMax + curr;
-			if (curr > curMax) {
-				curMax = curr;
-			}
+		if (nums.length == 0)
+			max = 0;
+		else {
+			max = nums[0];
 
-			if (curMax > max) {
-				max = curMax;
+			int curMax = nums[0];
+			for (int idxEnd = 1; idxEnd < nums.length; idxEnd++) {
+				int curr = nums[idxEnd];
+				curMax = curMax + curr;
+				if (curr > curMax) {
+					curMax = curr;
+				}
+
+				if (curMax > max) {
+					max = curMax;
+				}
 			}
 		}
+
 		System.out.println(max);
 		return max;
 	}
 
 	public static void main(String[] args) {
+		new MaxSubArray().maxSubArray(new int[]{-2, -1});
 //		new MaxSubArray().maxSubArray(new int[]{-1, 1, 2, -1, 5, 2, -1});
-		new MaxSubArray().maxSubArray(new int[]{3, -1, 1, 5, 1, 1, -1});
+//		new MaxSubArray().maxSubArray(new int[]{3, -1, 1, 5, 1, 1, -1});
 	}
 }
